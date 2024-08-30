c89f13e9dafdfbfe74a4bf2a603f43f470620237
Camila Alves <alvescamila87@gmail.com>
2024-08-30 18:53:47 -0300

    create api routes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,11 +3,13 @@ mongoose.Promise = global.Promise
 
 const mongoURI = 'mongodb://localhost:27017/todo';
 
-mongoose.connect(mongoURI, {
-    useNewURLParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error: ', err))
+const connectToDatabase = () => {
+    mongoose.connect(mongoURI, {
+        useNewURLParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error: ', err));
+};
 
-module.exports = mongoose;
+module.exports = connectToDatabase;
