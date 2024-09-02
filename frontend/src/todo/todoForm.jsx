313c9ae42efe3572a696e02bf8e5a56ca0575fd8
Camila Alves <alvescamila87@gmail.com>
2024-09-02 19:10:13 -0300

    create keyHandler to click ESC, Clear and add task by the keyboard

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
-export default props => (
+export default props => {
     // forma 1
     // <div role="form" className="todoForm">
     //     <div className="col-xs-12 col-sm-9 col-md-10">
@@ -17,33 +17,45 @@ export default props => (
     // </div>
 
     // forma 2: usando componente
-    <div role="form" className="todoForm">
-        <Grid cols="12 9 10">
-            <input 
-                id="description" 
-                className="form-control" 
-                placeholder="Add a new task"
-                onChange={props.handleChange}
-                value={props.description}
-            ></input>
-        </Grid>
 
-        <Grid cols="12 3 2">
-            <IconButton 
-                style="primary" 
-                icon="plus"
-                onClick={props.handleAdd}
-            />
-            <IconButton 
-                style='info'
-                icon='search'
-                onClick={props.handleSearch}
-            />
-            <IconButton
-                style="default"
-                icon="close"
-                onClick={props.handleClear}
-            />
-        </Grid>
-    </div>
-)
+    // adicionar teclas de atalho do teclado
+    const keyHandler = (e) => {
+        if(e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if(e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+    return (
+        <div role="form" className="todoForm">
+            <Grid cols="12 9 10">
+                <input 
+                    id="description" 
+                    className="form-control" 
+                    placeholder="Add a new task"
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                    value={props.description}
+                ></input>
+            </Grid>
+
+            <Grid cols="12 3 2">
+                <IconButton 
+                    style="primary" 
+                    icon="plus"
+                    onClick={props.handleAdd}
+                />
+                <IconButton 
+                    style='info'
+                    icon='search'
+                    onClick={props.handleSearch}
+                />
+                <IconButton
+                    style="default"
+                    icon="close"
+                    onClick={props.handleClear}
+                />
+            </Grid>
+        </div>
+    )
+}
