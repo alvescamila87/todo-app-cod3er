fd3fd444c95b51dab156d43cd3a7b251956808c0
Camila Alves <alvescamila87@gmail.com>
2024-09-01 22:15:54 -0300

    create header page component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => {
     return (
         <div>
-            <h1>About</h1>
+            <PageHeader name="About" small="Us"></PageHeader>
+
+            <h2>Our history</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est nesciunt odit corporis, voluptas commodi error cumque qui, porro ipsam, magnam praesentium aliquam rem obcaecati? Corporis dicta omnis quidem saepe facilis?</p>
+            <h2>Mission and Vision</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quaerat natus ea soluta. Delectus, eaque quam perspiciatis inventore voluptas molestias. Sequi magnam sed vero maiores quasi culpa rerum placeat laborum.</p>
+            <h2>Press</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum veniam debitis obcaecati corporis quae cupiditate ducimus asperiores vel expedita quibusdam? Neque necessitatibus dolorum, nulla delectus ab id ipsam unde praesentium!</p>
         </div>
     )
 }
