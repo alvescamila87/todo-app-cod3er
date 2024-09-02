e03893978fdfd5236752d27b2d7528d532b66b64
Camila Alves <alvescamila87@gmail.com>
2024-09-02 16:11:43 -0300

    create CORS config and bind to server

---
+++ backend/src/config/cors.js
@@ -0,0 +1,8 @@
+//middleware
+
+module.exports = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'OPTIONS', 'PATCH', 'DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origins, X-Requested-With, Content-Type, Accept')
+    next()
+}
